1d333c728ca585196ce28e1999eb04f3ab8132c9
wisnu kurniawan <wisnuko13@gmail.com>
2021-09-26 10:55:04 +0700

    Update default time

--- app/src/main/java/com/wisnu/kurniawan/composetodolist/foundation/extension/ToDoTaskExt.kt
+++ app/src/main/java/com/wisnu/kurniawan/composetodolist/foundation/extension/ToDoTaskExt.kt
@@ -46,7 +46,7 @@ fun ToDoTask.updatedTime(defaultDate: LocalDate, newLocalTime: LocalTime): Local
 }
 
 fun defaultTaskLocalTime(): LocalTime {
-    return LocalTime.of(23, 0)
+    return LocalTime.of(23, 59)
 }
 
 fun ToDoTask.isExpired(currentDate: LocalDateTime = DateTimeProviderImpl().now()): Boolean {
