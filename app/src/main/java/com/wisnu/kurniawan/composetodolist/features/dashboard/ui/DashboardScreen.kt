7d4192b21e079e2d98ab97990b22e384dcd7b8f3
wisnu kurniawan <wisnuko13@gmail.com>
2021-10-06 08:16:16 +0700

    Add search view

--- app/src/main/java/com/wisnu/kurniawan/composetodolist/features/dashboard/ui/DashboardScreen.kt
+++ app/src/main/java/com/wisnu/kurniawan/composetodolist/features/dashboard/ui/DashboardScreen.kt
@@ -1,22 +1,40 @@
 package com.wisnu.kurniawan.composetodolist.features.dashboard.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.NoteAdd
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.wisnu.kurniawan.composetodolist.R
 import com.wisnu.kurniawan.composetodolist.features.todo.main.ui.ItemMainState
@@ -26,7 +44,11 @@ import com.wisnu.kurniawan.composetodolist.features.todo.main.ui.ToDoMainViewMod
 import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.PgIcon
 import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.PgIconButton
 import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.PgPageLayout
+import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.PgTextField
 import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.PgTransparentFooter
+import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.SwipeSearch
+import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.SwipeSearchValue
+import com.wisnu.kurniawan.composetodolist.foundation.uicomponent.rememberSwipeSearchState
 import com.wisnu.kurniawan.composetodolist.runtime.navigation.HomeFlow
 import com.wisnu.kurniawan.composetodolist.runtime.navigation.ListDetailFlow
 import com.wisnu.kurniawan.composetodolist.runtime.navigation.SettingFlow
@@ -76,6 +98,119 @@ fun DashboardScreen(
     onScheduledTodayTask: () -> Unit,
     onScheduledTask: () -> Unit,
     onClickAllTask: () -> Unit,
+) {
+    var swipeSearchValue by remember { mutableStateOf(SwipeSearchValue.Closed) }
+    val swipeSearchState = rememberSwipeSearchState(swipeSearchValue)
+
+    val focusManager = LocalFocusManager.current
+    val focusRequest = remember { FocusRequester() }
+
+    LaunchedEffect(swipeSearchState.currentValue) {
+        if (swipeSearchState.currentValue == SwipeSearchValue.Opened) {
+            focusRequest.requestFocus()
+        } else {
+            focusManager.clearFocus()
+        }
+    }
+
+    val closeSearch = { swipeSearchValue = SwipeSearchValue.Closed }
+    if (swipeSearchState.currentValue == SwipeSearchValue.Opened) {
+        BackHandler(onBack = closeSearch)
+    }
+
+    SwipeSearch(
+        state = swipeSearchState,
+        onFling = {
+            swipeSearchValue = it
+        },
+        content = {
+            DashboardContent(
+                email,
+                todoData,
+                currentDate,
+                scheduledTodayTaskCount,
+                scheduledTaskCount,
+                allTaskCount,
+                onClickGroup,
+                onClickList,
+                onSwipeToDelete,
+                onScheduledTodayTask,
+                onScheduledTask,
+                onClickAllTask,
+                onSettingClick,
+                onAddNewListClick,
+                onAddNewGroupClick
+            )
+        },
+        searchSection = {
+            SearchSection(
+                modifier = Modifier.weight(1f),
+                focusRequester = focusRequest,
+                onCancelClick = closeSearch
+            )
+        },
+        searchBody = {
+
+        }
+    )
+}
+
+@Composable
+private fun SearchSection(
+    modifier: Modifier,
+    focusRequester: FocusRequester,
+    onCancelClick: () -> Unit
+) {
+    PgTextField(
+        value = "",
+        onValueChange = { },
+        placeholderValue = stringResource(R.string.todo_search),
+        modifier = modifier
+            .height(50.dp)
+            .focusRequester(focusRequester),
+        shape = MaterialTheme.shapes.large,
+        textStyle = MaterialTheme.typography.caption.copy(fontSize = 14.sp),
+        leadingIcon = {
+            PgIcon(
+                imageVector = Icons.Rounded.Search
+            )
+        }
+    )
+
+    Spacer(Modifier.width(8.dp))
+
+    Column {
+        TextButton(
+            onClick = onCancelClick
+        ) {
+            Text(
+                text = stringResource(R.string.todo_cancel),
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.onSurface
+            )
+        }
+
+        Spacer(Modifier.height(6.dp))
+    }
+}
+
+@Composable
+private fun DashboardContent(
+    email: String,
+    todoData: List<ItemMainState>,
+    currentDate: String,
+    scheduledTodayTaskCount: String,
+    scheduledTaskCount: String,
+    allTaskCount: String,
+    onClickGroup: (ItemMainState.ItemGroup) -> Unit,
+    onClickList: (ItemMainState.ItemListType) -> Unit,
+    onSwipeToDelete: (ItemMainState.ItemListType) -> Unit,
+    onScheduledTodayTask: () -> Unit,
+    onScheduledTask: () -> Unit,
+    onClickAllTask: () -> Unit,
+    onSettingClick: () -> Unit,
+    onAddNewListClick: () -> Unit,
+    onAddNewGroupClick: () -> Unit
 ) {
     PgPageLayout {
         Text(
