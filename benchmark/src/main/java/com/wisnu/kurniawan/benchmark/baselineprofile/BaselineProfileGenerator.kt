d63c17c6c7a9a27cbb8294649e071762136f2a82
wisnu kurniawan <wisnuko13@gmail.com>
2022-05-14 13:15:17 +0700

    Integrate macro benchmark and base profiler

---
+++ benchmark/src/main/java/com/wisnu/kurniawan/benchmark/baselineprofile/BaselineProfileGenerator.kt
@@ -0,0 +1,35 @@
+package com.wisnu.kurniawan.benchmark.baselineprofile
+
+import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.uiautomator.By
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Generates a baseline profile which can be copied to `app/src/main/baseline-prof.txt`.
+ */
+@ExperimentalBaselineProfilesApi
+class BaselineProfileGenerator {
+    @get:Rule
+    val baselineProfileRule = BaselineProfileRule()
+
+    @Test
+    fun startup() =
+        baselineProfileRule.collectBaselineProfile(
+            packageName = "com.wisnu.kurniawan.composetodolist"
+        ) {
+            pressHome()
+            // This block defines the app's critical user journey. Here we are interested in
+            // optimizing for app startup. But you can also navigate and scroll
+            // through your most important UI.
+            startActivityAndWait()
+            device.waitForIdle()
+
+            device.run {
+                findObject(By.text("Today"))
+                    .click()
+                waitForIdle()
+            }
+        }
+}
